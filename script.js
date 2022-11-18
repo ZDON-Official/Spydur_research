ae0ad1c636fb469f2c19b60055a4e287c4811c24
Zohaib <zrana1214@gmail.com>
2022-11-18 14:50:49 -0500

    working on search bar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-$(document).ready(function () {
-    $("#fullview").fullView();
-});
+// $(document).ready(function () {
+//     $("#fullview").fullView();
+// });
+
+var search_bar = document.getElementById('search_bar_main')
+
+
+function check_input(e){
+    console.log('checking search bar')
+    document.location.href = './departments.html'
+    e.
+}
+
+search_bar.addEventListener('blur', check_input())
